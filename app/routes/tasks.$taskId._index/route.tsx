7e7c50b8c7d3007a5a0330f1560394b56ecf981b
kanemototomoki <kanemototomoki@gmail.com>
2023-12-19 15:17:36 +0900

    add: apge

---
+++ app/routes/tasks.$taskId._index/route.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+      <h1>tasks/:taskId</h1>
+    </div>
+  );
+}
---
+++ app/routes/tasks._index/route.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+  return (
+    <div
+      style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}
+      >
+        <h1>tasks</h1>
+    </div>
+  );
+}
